f0a7fdebd2887575b2a8a81cc770811639707069
tronghoainguyen5-collab <tronghoainguyen5@gmail.com>
2026-03-26 00:59:12 +0700

    Delete src directory

--- src/services/CategoryService.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "../models/Category.js";
-import { ApiService } from "./ApiService.js";
-
-export class CategoryService extends ApiService {
-   async getAll(): Promise<Category[]> {
-      const data: Category[] = await this.get<Category>("categories");
-      return data.map((c: any) => new Category(c.id, c.name));
-   }
-}
